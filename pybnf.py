363e908126f25f5d7ad6253361d1e1eedf98c8e8
Full Metal <tyler9725@gmail.com>
2014-02-07 23:05:40 -0600

    new approach to this tokenizer.
    the last one had my head spinning tbh.

--- pybnf.py
+++ pybnf.py
@@ -7,48 +7,29 @@ class Tokenizer:
 	def __init__(self,stoplist):
 		self.stoplist = stoplist
 	def __call__(self,text):
-		position,end = 0, len(text)
-		while position<end:
-			start = position
-			while text[position] not in self.stoplist: position+=1
-			else: yield text[position],None
-			if position>start: yield text[position],text[start:position]
-			position += 1
+		def find_all_occurences(haystack,needle):
+			last_position = 0
+			while True:
+				last_positionhaystack.find(needle,last_position)
+				if last_position<0:break
+				yield last_position
+			return range(0)
+		occurences,sorted_points = [(tuple(find_all_occurences(text,t)),t) for t in self.stoplist],[]
+		for points,token in occurences:
+			sorted_points.extend((token,p) for p in points)
+		return sorted(sorted_points)
 
 class GrammarParser:
 	rules = {}
-	tokenizer = Tokenizer("'\"(){}[]*;|=, ")
+	tokenizer = Tokenizer("'\"\\(){}[]*;|=, ")
 	def __init__(self,grammar_text):
-		self.grammar_text = convert_whitespace(grammar_text)
+		self.grammar_text = convert_whitespace(grammar_text).strip()
 		self.rules = {}
 	def build_grammar(self):
-		grammar,building_token,current_rule = {},None,None
-		for token, data in self.tokenizer(self.grammar_text):
-			if token==' ':
-				if building_token is not None:
-					if data is None: building_token += ' '
-					else: building_token += data
-				elif data is not None: building_token = data
-			elif token=='=':#the currently building token must be a rule name.
-				current_rule = building_token.strip()
-				grammar[current_rule] = [[]]
-				building_token = None
-			elif token=='|':
-				grammar[current_rule].append([])
-			elif token==';':
-				current_rule = None
-			elif token=='"':
-				if building_token is not None:#closing a string.
-					building_token += (data.rstrip() if data is not None else "") + '"'
-					print("Closing literal:",building_token)
-				else:
-					print("Opening literal.")
-					building_token = '"'
-			elif token==',':
-				if data is not None and building_token is not None:
-					building_token+=data.rstrip()
-				grammar[current_rule][-1].append(building_token)
-				building_token = None
+		grammar,building_token,current_rule,escaped = {},None,None,False
+		(tokens,positions),index=tuple(zip(*self.tokenizer(self.grammar_text))),0
+		while index<len(tokens):
+
 		return grammar
 
 if __name__=="__main__":
