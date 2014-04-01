4101662c9917961b630f05f4817e5e4d5ab61ffb
system_user <tyler9725@gmail.com>
2014-04-01 11:34:43 -0700

    regexes

--- pybnf.py
+++ pybnf.py
@@ -1,35 +1,18 @@
+#!/usr/bin/python
 from re import compile as Regex, sub as regex_replace
+import re
 
 get_pieces = lambda src,sp:[p.strip() for p in src.split(sp) if len(p.strip())>0]
 convert_whitespace = lambda src:regex_replace("\s\s+"," ",src)
 
-class Tokenizer:
-	def __init__(self,stoplist):
-		self.stoplist = stoplist
-	def __call__(self,text):
-		def find_all_occurences(haystack,needle):
-			last_position = 0
-			while True:
-				last_positionhaystack.find(needle,last_position)
-				if last_position<0:break
-				yield last_position
-			return range(0)
-		occurences,sorted_points = [(tuple(find_all_occurences(text,t)),t) for t in self.stoplist],[]
-		for points,token in occurences:
-			sorted_points.extend((token,p) for p in points)
-		return sorted(sorted_points)
-
 class GrammarParser:
-	rules = {}
-	tokenizer = Tokenizer("'\"\\(){}[]*;|=, ")
+	rule_searcher = Regex('([^\s][^=]+)\s*=.+;',re.MULTILINE)
 	def __init__(self,grammar_text):
 		self.grammar_text = convert_whitespace(grammar_text).strip()
-		self.rules = {}
 	def build_grammar(self):
-		grammar,building_token,current_rule,escaped = {},None,None,False
-		(tokens,positions),index=tuple(zip(*self.tokenizer(self.grammar_text))),0
-		while index<len(tokens):
-
+		grammar = {}
+		for match in self.rule_searcher.finditer(self.grammar_text):
+			print(match.group(1))
 		return grammar
 
 if __name__=="__main__":
